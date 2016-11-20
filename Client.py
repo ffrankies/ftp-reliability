9ff888216cbd0f1fb241411c6be88e8aa02313a8
Frank <wanyefrank@live.com>
2016-11-20 22:00:59 +0000

    This instantiates some sockets and thread for communication
    The server passes clientAddresses to a function that will handle them
    
    The client creates a socket and prompts the user to input a file name to use

--- Client.py
+++ Client.py
@@ -1,1 +1,12 @@
 import SlidingWindow as window
+import socket
+
+port = input("Please enter a port for connection: ")
+print ("Creating connection to the server")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('localhost', port))
+
+#Send the file request to the server
+while 1:
+    filename = input("Please enter a filename: ")
+    
--- Server.py
+++ Server.py
@@ -1,1 +1,34 @@
 import SlidingWindow as window
+import socket
+from threading import Thread
+
+port = input("Please enter a port for connection: ")
+print ("Creating server socket.")
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serversocket.bind((socket.gethostname(), port))
+serversocket.listen(2)
+
+#Create a socket and pass it on to a thread
+while 1:
+    (clientsocket, address) = serversocket.accept()
+    t = Thread(target=HandleClients, args=(address,))
+    t.start()
+    
+
+def HandleClients(address):
+    '''
+    Handles the transfer of the files from server to clientsocket
+    
+    :type address: socket address
+    :param address: the address of the client passed by the thread
+    
+    TO-DO
+    - Receive file request from the client
+    - Read from local file and save in memory
+    - Instantiate a sliding window to keep track of packets sent
+    - Finish send and potentially wait for another client request
+    
+    '''
+    
+    
